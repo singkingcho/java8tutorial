7d0d21cfa6bcc6be2cb006482cf03b7b40867746
singkingcho <singkingcho@163.com>
2018-05-23 11:52:42 +0800

    第二个项目添加测试

---
+++ chapter2/src/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * @Author 邱道长
+ * @Date 2018/5/23
+ * @Description
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("很好很好");
+    }
+}
