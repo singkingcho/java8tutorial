2fb8c95efaa281e7961a05063816a5de54adfa7d
singkingcho <singkingcho@163.com>
2018-05-23 12:02:18 +0800

    This is for maven....

---
+++ S01EP02/src/main/java/MavenDemo1.java
@@ -0,0 +1,13 @@
+/**
+ * @Author 邱道长
+ * @Date 2018/5/23
+ * @Description
+ */
+public class MavenDemo1 {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("This is for you");
+    }
+}
