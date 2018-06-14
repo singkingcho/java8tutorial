6571e4cb74c068598c88be6c17f66d92e168871a
singkingcho <singkingcho@163.com>
2018-06-14 11:23:07 +0800

    remove outer all

--- S01EP01/src/Korean.java
+++
@@ -1,6 +0,0 @@
-public class Korean{
-	
-	public static void main (String[] args) {
-		System.out.println("°ô×Ó³ÔËá²Ë");
-	}
-}
--- S01EP01/src/Println.java
+++
@@ -1,9 +0,0 @@
-public class Println{
-	
-	public static void main (String[] args){
-		System.out.println("I love baby");
-		System.out.println("I love baby's Sister");
-	}
-	
-	
-}
